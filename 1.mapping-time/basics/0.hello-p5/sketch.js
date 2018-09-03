11b052711342f898048c584d44dda54dd81c69fe
Christian Swinehart <drafting@samizdat.cc>
2018-09-03 12:13:14 -0400

    inverted palette

--- 1.mapping-time/basics/0.hello-p5/sketch.js
+++ 1.mapping-time/basics/0.hello-p5/sketch.js
@@ -2,7 +2,7 @@
 function setup() {
   createCanvas(512, 256); // set the size of the canvas
   frameRate(60) // redraw the screen 60 times per second
-  background(255) // fill the canvas with white pixels
+  background(0) // fill the canvas with black pixels
   print("Hello, javascript console.")
 }
 
@@ -13,17 +13,17 @@ function draw() {
   textSize(72)
   noStroke()
 
-  // pick random values to decide on location, size, and color of the next dot
+  // pick random values to decide on location, size, and opacity of the next dot
   var x = random(width),
-      y = randomGaussian(height/2, height/6),
-      r = randomGaussian(20, 10),
-      c = Math.floor(random(150));
+      y = randomGaussian(height/2, height/8),
+      r = random(3, 30),
+      c = color(255, random(100));
 
   // set the color & draw the dot
   fill(c)
   ellipse(x, y, r)
 
-  // switch back to drawing in white and print our greeting on top
-  fill(255)
+  // switch back to drawing in black and print our greeting on top
+  fill(0)
   text("Hello, world.", width/2, height/2 + textSize()/3)
 }
