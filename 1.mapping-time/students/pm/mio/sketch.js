5054a308910b61b138fae7418a7a9b3866d48cdb
miopio <mioakasako@gmail.com>
2018-09-26 16:49:46 -0400

    update day and night bars for medium variant

--- 1.mapping-time/students/pm/mio/sketch.js
+++ 1.mapping-time/students/pm/mio/sketch.js
@@ -68,22 +68,38 @@ function draw() {
 //create variable for sleep cycle count
   var cyclecount = now.hours / 1.6
 
-  //create circadian rhythm columns
+    //create circadian rhythm columns
+  //make day column go up between 6am - 6pm
+  //make night column go down betwen 6pm - 6am
+
+var dayHeight = 0
+var nightHeight = 0
+  if (now.progress.day <= .25){
+    dayHeight = 0
+    nightHeight = maxHeight * (now.progress.day + .25)/.5
+  }
+  if (now.progress.day > .25 && now.progress.day <= .75){
+    dayHeight = maxHeight * (now.progress.day - .25)/.5
+    nightHeight = 0
+  }     
+  if (now.progress.day > .75){
+    dayHeight = 0
+    nightHeight = maxHeight * (now.progress.day - .75)/.5
+  }
 
   //create DAY column
   var boxwidthDay = 54
   var boxspacing = 6
 
-  var dayHeight = now.progress.halfday * maxHeight 
-    if(now.hours <= 11){
-      fill(0.1, 1, 1)}
-    else {fill(0)}
+    if(nightHeight > 0){fill(0)}
+      else{fill(0.1,1,1)}
+    
   rect(143+boxspacing/2, y+3, boxwidthDay, maxHeight - dayHeight)
  
-  //NIGHT column
-  var nightHeight = now.progress.day * maxHeight 
-  fill(0.55, 1, now.progress.day)
+  //create NIGHT column
+  fill(0.55,1,1)  
   rect(143+boxspacing/2+boxwidthDay+boxspacing, y+3, boxwidthDay, nightHeight)
+
 
   
   //create sleep cycle boxes
@@ -257,3 +273,5 @@ function draw() {
   rect(394+boxspacing/2+ boxwidthFSH*4+boxspacing*4, y+3, barWidth, monthHeight)
 
 }
+
+
