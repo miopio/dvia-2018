fc5652951fbf0dfa3edc7c84feff0119bcb25321
Dan Ran <randanfx@gmail.com>
2018-10-10 12:04:52 -0400

    Rough sketch assignment 2

--- 2.mapping-quantities/students/am/dan/concept-1.js
+++ 2.mapping-quantities/students/am/dan/concept-1.js
@@ -1,11 +1,67 @@
-var data
-
+var usaNdata;
+var usaTrustTable;
+var trustRate ;
 function preload(){
-  data = loadJSON('data/totals.json')
+  usaNdata = loadJSON('data/totals.json');
+  usaTrustTable= loadTable('data/public-trust-in-government.csv', 'csv', 'header');
+   img = loadImage('data/nuclear.svg');
 }
 
 function setup(){
-  createCanvas(800, 600)
+  createCanvas(2000, 600);
+  background(200);
+  print(usaNdata.tests["United States"]);
+  print(usaTrustTable.getRowCount() + ' total rows in table');
+  print(usaTrustTable.getColumnCount() + ' total columns in table');
+  print(usaTrustTable.getColumn('Trust in Government (%)'));
 
-  print(data)
+  trustRate= usaTrustTable.getColumn('Trust in Government (%)');
+  print(trustRate[1]);
+
+  drawNuclear();
+  drawTrustLine();
+
+//image(img, 0, 0, 10, 10);
+
+}
+
+function drawTrustLine(){
+  var trusts=[];
+  line(11*23+50,height,11*23+50,height-300);
+  text('100%', 10*23+15, height-295);
+  text('0%', 10*23+15, height-5);
+  text('Public trust in government, United States', width/3, height-10);
+  for(var i=0; i<trustRate.length; i++){
+    var trust = map (trustRate[i],0,100, 0,300);
+    trusts.push(trust);
+  }
+  for(var i=0; i<trusts.length; i++){
+    line(13*23+25+i*23, height-trusts[i], 13*23+25+(i+1)*23, height-trusts[i+1]);
+    textSize(8);
+    text(usaTrustTable.getColumn('Year')[i], (1958-1945)*23+25+i*23, height-300);
+  }
+
+print(trusts);
+}
+
+function drawNuclear(){
+  var xWidth= 23;
+  var x=25;
+  for (var i =0 ; i<usaNdata.years.length; i++){
+      textSize(8);
+      text (usaNdata.years[i], x, 50)
+
+      if (usaNdata.tests["United States"][i]>0){
+     image(img, x, 100, 10, usaNdata.tests["United States"][i]);
+   }
+     x+=xWidth;
+
+  }
+
+
+  // for (var testYear in usaNdata.tests){
+  //   text (testYear, x, 100)
+  //   x+=xWidth;
+  // }
+
 }
--- 2.mapping-quantities/students/am/dan/concept-2.js
+++ 2.mapping-quantities/students/am/dan/concept-2.js
@@ -1,11 +1,45 @@
-var data
-
+var data;
+var stateBattle;
+var StateBased;
 function preload(){
   data = loadJSON('data/totals.json')
+  stateBattle= loadTable('data/state-based-battle-related-deaths-per-100000-since-1946.csv', 'csv', 'header');
 }
 
 function setup(){
-  createCanvas(800, 600)
+  createCanvas(2000, 600)
 
-  print(data)
+   print(stateBattle.getRows(['State-based conflicts']));
+  print(stateBattle.findRows('State-based conflicts', 'Entity'));
+  stateBased= stateBattle.findRows('State-based conflicts', 'Entity');
+  print(stateBased[0].obj['Rate of violent deaths in conflicts and one-sided violence (per 100,000)']);
+  print(stateBased[1].arr[2])
+
+  drawStateBattle();
+}
+
+
+function drawStateBattle(){
+var deaths=[];
+var x =25;
+var xWidth= 20;
+text ('Rate of Violent Deaths in State Based Conflicts', 20, 30)
+  for (var i=0; i< stateBased.length; i++){
+      textSize(8);
+      text (stateBased[i].arr[2], x, 100)
+
+    x+=xWidth;
+      var death =map(stateBased[i].arr[3],0.0000000,22, 20,300);
+      deaths.push(death);
+
+  }
+  scale(1, -1);
+  translate(0, -height);
+  for (var i=0; i< deaths.length; i++){
+    rect(25+i*20, 150, 15, deaths[i]);
+
+  }
+
+
+
 }
