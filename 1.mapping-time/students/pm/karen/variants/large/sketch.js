95a6d1d82bba7642fe0cdd2ac6bb1fcd00d128bd
karen <karenjtian@gmail.com>
2018-10-11 10:40:49 -0400

    process P2 pdf

--- 1.mapping-time/students/pm/karen/variants/large/sketch.js
+++ 1.mapping-time/students/pm/karen/variants/large/sketch.js
@@ -11,6 +11,7 @@ var cx, cy; // center position of canvas
 var secondsRadius
 var minutesRadius
 var hoursRadius
+var daysRadius
 var clockDiameter
 
 var bg = 200; 
@@ -21,18 +22,18 @@ function setup() {
   createCanvas(500, 500)
   stroke(255)
 
-  var radius = min(width, height) / 2.1; // this is the maximum possible radius
+  var radius = min(width, height) / 2.3; // this is the maximum possible radius
   secondsRadius = radius * 0.4
   minutesRadius = radius * 0.35
   hoursRadius = radius * 0.3
-  daysRadius = radius * 0.25
+  daysRadius = radius * 0.1
 
   cx = width / 2
   cy = height / 2
 }
 
 function draw() {
-  background(bg, 5)
+  background(bg, 10)
 
 
   // Angles for sin() and cos() start at 3 o'clock
@@ -61,24 +62,24 @@ function draw() {
   var dx = mx + cos(d)*daysRadius
   var dy = my + sin(d)*daysRadius
 
-  //CIRCLES********
-  //draw circle seconds 
-  fill(bg, 0)
-  stroke('black')
-  strokeWeight(2)
-  ellipse(cx, cy, secondsRadius*2, secondsRadius*2)
+  // //CIRCLES********
+  // //draw circle seconds 
+  // fill(bg, 0)
+  // stroke('black')
+  // strokeWeight(2)
+  // ellipse(cx, cy, secondsRadius*2, secondsRadius*2)
 
-  //draw circle minutes
-  fill(bg, 0)
-  stroke('black')
-  strokeWeight(2)
-  ellipse(sx, sy, minutesRadius*2, minutesRadius*2)
+  // //draw circle minutes
+  // fill(bg, 0)
+  // stroke('black')
+  // strokeWeight(2)
+  // ellipse(sx, sy, minutesRadius*2, minutesRadius*2)
 
-  //draw circle hour
-  fill(bg, 0)
-  stroke('black')
-  strokeWeight(2)
-  ellipse(mx, my, hoursRadius*2, hoursRadius*2)
+  // //draw circle hour
+  // fill(bg, 0)
+  // stroke('black')
+  // strokeWeight(2)
+  // ellipse(mx, my, hoursRadius*2, hoursRadius*2)
 
   //HANDS*******
   // Draw the second hand 
@@ -88,17 +89,17 @@ function draw() {
 
   // draw the minute hand 
   stroke('black')
-  strokeWeight(5)
+  strokeWeight(3)
   line(sx, sy, mx, my)
 
   // draw the hour hand 
   stroke('black')
-  strokeWeight(10)
+  strokeWeight(4)
   line(mx, my, hx, hy)
 
   // draw the days hand 
   stroke('black')
-  strokeWeight(15)
+  strokeWeight(5)
   line(hx, hy, dx, dy)
 }
 
