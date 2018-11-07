3fcec98a68b5816718e84c95a96e02f7541d356c
Zui Chen <zuic@umich.edu>
2018-11-07 10:00:56 -0500

    Added a day slider

--- 3.mapping-space/students/am/zui/sketch.js
+++ 3.mapping-space/students/am/zui/sketch.js
@@ -32,10 +32,17 @@ function preload() {
 
 function setup() {
     // first, call our map initialization function (look in the html's style tag to set its dimensions)
-    setupMap()
-
+    // createCanvas(windowWidth,windowHeight);
+    // background('rgb(30,30,30)');
     // next, draw our p5 diagram that complements it
-    createCanvas(windowWidth, windowHeight);
+
+    setupMap();
+    //
+    // background('rgb(30,30,30)');
+    slider = createSlider(1,31,day(),1);
+    slider.style('color','#999');
+    slider.position(windowWidth-200,10);
+    slider.changed(filterDataPoints);
     // background(222);
     //
     // fill(0)
@@ -44,6 +51,8 @@ function setup() {
     // text(`Plotting ${table.getRowCount()} seismic events`, 20, 40)
     // text(`Largest Magnitude: ${getColumnMax("mag")}`, 20, 60)
     // text(`Greatest Depth: ${getColumnMax("depth")}`, 20, 80)
+    // createCanvas(windowWidth, windowHeight);
+
 }
 
 function setupMap(){
@@ -68,7 +77,6 @@ function setupMap(){
     }).addTo(mymap);
 
 
-
     // call our function (defined below) that populates the maps with markers based on the table contents
     drawDataPoints();
 
@@ -88,15 +96,24 @@ function drawDataPoints(){
     strokeWeight(0.1);
     // stroke(255,0,0);
 
-
-    // get the two arrays of interest: depth and magnitude
     depths = table.getColumn("depth");
     magnitudes = table.getColumn("mag");
     latitudes = table.getColumn("latitude");
     longitudes = table.getColumn("longitude");
     times = table.getColumn("time");
+    days = times.map(i => parseInt(i.slice(8, 10)));
 
-    days = times.map(i => i.substr(0,9));
+    // var sigPoints=[];
+    // for(var i=0; i<depths.length; i++){
+    //   sigPoints.push([latitudes[i], longitudes[i]]);
+    // };
+    sigdepths = sig.getColumn("depth");
+    sigmagnitudes = sig.getColumn("mag");
+    siglatitudes = sig.getColumn("latitude");
+    siglongitudes = sig.getColumn("longitude");
+    sigtimes = sig.getColumn("time");
+    sigdays = sigtimes.map(i => parseInt(i.slice(8, 10)));
+
 
     // get minimum and maximum values for both
     magnitudeMin = 0.0;
@@ -109,10 +126,26 @@ function drawDataPoints(){
 
     // cycle through the parallel arrays and add a dot for each event
     for(var i=0; i<depths.length; i++){
+
+          var circle = L.circle([latitudes[i],longitudes[i]], {
+              color: '#3388ff',      // the dot stroke color
+              opacity: magnitudes[i]/100,  // use some transparency so we can see overlaps
+              radius: 5
+              // day: days[i]
+          });
+
+        // place it on the map
+        circle.addTo(mymap);
+
+        // save a reference to the circle for later
+        circles.push(circle)
+    }
+
+    for(var i=0; i<sigdepths.length; i++){
         // create a new dot
-        var circle = L.circle([latitudes[i], longitudes[i]], {
-            color: '#3388ff',      // the dot stroke color
-            opacity: depths[i]/100,  // use some transparency so we can see overlaps
+        var circle = L.circle([siglatitudes[i],siglongitudes[i]], {
+            color: '#8b0000',      // the dot stroke color
+            opacity: sigmagnitudes[i]/getColumnMax('sigmagnitudes'),  // use some transparency so we can see overlaps
             radius: 5
             // day: days[i]
         });
@@ -125,6 +158,77 @@ function drawDataPoints(){
     }
 }
 
+function filterDataPoints(){
+    removeAllCircles();
+
+    // stroke('white');
+    // strokeWeight(2);
+    // textSize(10);
+    // text(slider.value(), windowWidth-200, 100);
+    // console.log(slider.value());
+
+    strokeWeight(0.1);
+    // stroke(255,0,0);
+    // console.log(1);
+    // depths = table.getColumn("depth");
+    // magnitudes = table.getColumn("mag");
+    // latitudes = table.getColumn("latitude");
+    // longitudes = table.getColumn("longitude");
+    // times = table.getColumn("time");
+    // days = times.map(i => i.substr(8,9));
+    // console.log(2);
+
+    // sigdepths = sig.getColumn("depth");
+    // sigmagnitudes = sig.getColumn("mag");
+    // siglatitudes = sig.getColumn("latitude");
+    // siglongitudes = sig.getColumn("longitude");
+    // sigtimes = sig.getColumn("time");
+    // sigtimes = sig.getColumn("time");
+    // sigdays = sigtimes.map(i => i.substr(8,9));
+
+    // get minimum and maximum values for both
+    // magnitudeMin = 0.0;
+    // magnitudeMax = getColumnMax("mag");
+    // // console.log('magnitude range:', [magnitudeMin, magnitudeMax])
+    //
+    // depthMin = 0.0;
+    // depthMax = getColumnMax("depth");
+    // console.log('depth range:', [depthMin, depthMax])
+
+    // cycle through the parallel arrays and add a dot for each event
+    for(var i=0; i<depths.length; i++){
+        if (days[i]==slider.value()){
+          var circle = L.circle([latitudes[i],longitudes[i]], {
+              color: '#3388ff',      // the dot stroke color
+              opacity: magnitudes[i]/100,  // use some transparency so we can see overlaps
+              radius: 5
+              // day: days[i]
+          });
+          console.log(days[i]);
+          // place it on the map
+          circle.addTo(mymap);
+          // save a reference to the circle for later
+          circles.push(circle);
+        };
+
+    }
+
+    for(var i=0; i<sigdepths.length; i++){
+        if (sigdays[i]==slider.value()){
+          var circle = L.circle([siglatitudes[i],siglongitudes[i]], {
+              color: '#8b0000',      // the dot stroke color
+              opacity: sigmagnitudes[i]/getColumnMax('sigmagnitudes'),  // use some transparency so we can see overlaps
+              radius: 5
+              // day: days[i]
+          });
+          // place it on the map
+          circle.addTo(mymap);
+          // save a reference to the circle for later
+          circles.push(circle)
+        };
+
+    }
+}
 
 function removeAllCircles(){
     // remove each circle from the map and empty our array of references
