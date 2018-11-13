6cf4532ee0fc6681ef09dcaf5604cd53f1dce585
andrew_l <andrewlevins@gmail.com>
2018-11-12 23:42:26 -0500

    assignment 3 progress

--- 3.mapping-space/students/am/andrew/sketch.js
+++ 3.mapping-space/students/am/andrew/sketch.js
@@ -5,6 +5,8 @@ var depths;
 // an array for lat & long
 var latitudes, longitudes;
 
+var magError, depthError, horizontalError;
+
 // minimum and maximum values for magnitude and depth
 var magnitudeMin, magnitudeMax;
 var depthMin, depthMax;
@@ -58,12 +60,13 @@ function setupMap() {
   // load a set of map tiles – choose from the different providers demoed here:
   // https://leaflet-extras.github.io/leaflet-providers/preview/
   L.tileLayer(
-    "https://stamen-tiles-{s}.a.ssl.fastly.net/toner/{z}/{x}/{y}{r}.{ext}?access_token={accessToken}",
+    "https://stamen-tiles-{s}.a.ssl.fastly.net/toner-background/{z}/{x}/{y}{r}.{ext}?access_token={accessToken}",
     {
       // attribution:
       //   'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
       minZoom: 2,
       maxZoom: 10,
+      opacity: 0.2,
       id: "mapbox.streets",
       ext: "png",
       accessToken:
@@ -84,51 +87,46 @@ function drawDataPoints() {
   magnitudes = table.getColumn("mag");
   latitudes = table.getColumn("latitude");
   longitudes = table.getColumn("longitude");
+  magError = table.getColumn("magError");
+  depthError = table.getColumn("depthError");
+  horizontalError = table.getColumn("horizontalError");
+
+  // convert to numbers
+  for (var x = 0; x < depths.length; x++) {
+    depths[x] = +depths[x];
+  }
+  for (var x = 0; x < magnitudes.length; x++) {
+    magnitudes[x] = +magnitudes[x];
+  }
+  for (var x = 0; x < latitudes.length; x++) {
+    latitudes[x] = +latitudes[x];
+  }
+  for (var x = 0; x < longitudes.length; x++) {
+    longitudes[x] = +longitudes[x];
+  }
+  for (var x = 0; x < magError.length; x++) {
+    magError[x] = +magError[x];
+  }
+  for (var x = 0; x < depthError.length; x++) {
+    depthError[x] = +depthError[x];
+  }
+  for (var x = 0; x < horizontalError.length; x++) {
+    horizontalError[x] = +horizontalError[x];
+  }
+
+  // get avg. values for both
+  var avgMag = magnitudes.reduce((a, b) => a + b, 0) / magnitudes.length;
+  var avgDepth = depths.reduce((a, b) => a + b, 0) / depths.length;
 
   // get minimum and maximum values for both
   magnitudeMin = 0.0;
   magnitudeMax = getColumnMax("mag");
-  console.log("magnitude range:", [magnitudeMin, magnitudeMax]);
+  // console.log("magnitude range:", [magnitudeMin, magnitudeMax]);
 
   depthMin = 0.0;
   depthMax = getColumnMax("depth");
-  console.log("depth range:", [depthMin, depthMax]);
+  // console.log("depth range:", [depthMin, depthMax]);
 
-  var numberOfShades = 4;
-  var palette = Brewer.sequential(
-    "OrRd",
-    numberOfShades,
-    magnitudeMin,
-    magnitudeMax
-  );
-
-  // cycle through the parallel arrays and add a dot for each event
-  for (var i = 0; i < depths.length; i++) {
-    // set color
-    var color = palette.colorForValue(magnitudes[i]);
-
-    // create a new dot
-    var circle = L.circle([latitudes[i], longitudes[i]], {
-      color: color, // the dot stroke color
-      // fillColor: "#f03", // the dot fill color
-      fillColor: color,
-      fillOpacity: 0.8, // use some transparency so we can see overlaps
-      // radius: magnitudes[i] * 40000
-      radius: depths[i] * 2000
-    });
-    // place it on the map
-    circle.addTo(mymap);
-    // save a reference to the circle for later
-    circles.push(circle);
-  }
-
-  // // play around with poly shape
-  // var latlngs = [[37, -109.05], [41, -109.03], [41, -102.05], [37, -102.04]];
-  // var polygon = L.polygon(latlngs, { color: "red" }).addTo(mymap);
-  // // zoom the map to the polygon
-  // // mymap.fitBounds(polygon.getBounds());
-
-  // create a light blue polyline from an array of LatLng points
   var latlngs = [
     [latitudes[0], longitudes[0]],
     [latitudes[1], longitudes[1]],
@@ -144,9 +142,171 @@ function drawDataPoints() {
     [latitudes[11], longitudes[11]],
     [latitudes[12], longitudes[12]]
   ];
+
+  // create a red polygon from an array of LatLng points
+  // var ls = [
+  //   [latitudes[0], longitudes[0]],
+  //   [41, -109.03],
+  //   [41, -102.05],
+  //   [37, -102.04]
+  // ];
+
+  var numberOfShades = 9;
+  var palette = Brewer.sequential("Reds", numberOfShades, 0, 13);
+
+  //create the circles
+  for (var i = 0; i < latlngs.length; i++) {
+    // set color
+    var color = palette.colorForValue(i);
+    // create a new dot
+    var circleEvent = L.circle([latitudes[i], longitudes[i]], {
+      color: "#67000d", // the dot stroke color
+      // fillColor: "#f03", // the dot fill color
+      fillColor: color,
+      fillOpacity: 1, // use some transparency so we can see overlaps
+      // radius: magnitudes[i] * 40000
+      // radius: depths[i] * 2000
+      radius: 80000
+    });
+    // place it on the map and make clickable
+    circleEvent.addTo(mymap);
+    circleEvent.on("click", onCircleClick);
+    // save a reference to the circle for later
+    circles.push(circleEvent);
+
+    // create a new dot for magError
+    var circleMagErr = L.circle(
+      [latitudes[i] + magnitudes[i] * 0.8, longitudes[i]],
+      {
+        color: "blue", // the dot stroke color
+        fillColor: "blue",
+        fillOpacity: 0.3,
+        radius: 300000 * magError[i]
+      }
+    );
+    // place it on the map and make clickable
+    circleMagErr.addTo(mymap);
+    circleMagErr.on("click", onCircleClick);
+
+    // create a new dot for depthError
+    var circleDepthErr = L.circle(
+      [latitudes[i] - depths[i] * 0.2, longitudes[i]],
+      {
+        color: "green", // the dot stroke color
+        fillColor: "green",
+        fillOpacity: 0.3,
+        radius: 20000 * depthError[i]
+      }
+    );
+    // place it on the map and make clickable
+    circleDepthErr.addTo(mymap);
+    circleDepthErr.on("click", onCircleClick);
+  }
+
+  // create the polylines
+  for (i = 0; i < latlngs.length; i++) {
+    let adjust = 0.5;
+    var horizontalLineRight = [
+      [latitudes[i], longitudes[i]],
+      [latitudes[i], longitudes[i] + horizontalError[i] * adjust]
+    ];
+    var horizontalLineLeft = [
+      [latitudes[i], longitudes[i]],
+      [latitudes[i], longitudes[i] - horizontalError[i] * adjust]
+    ];
+    var pointMag = [
+      [latitudes[i], longitudes[i] + horizontalError[i] * adjust],
+      [latitudes[i] + magnitudes[i] * 0.8, longitudes[i]],
+      [latitudes[i], longitudes[i] - horizontalError[i] * adjust]
+    ];
+    var pointDepth = [
+      [latitudes[i], longitudes[i] + horizontalError[i] * adjust],
+      [latitudes[i] - depths[i] * 0.2, longitudes[i]],
+      [latitudes[i], longitudes[i] - horizontalError[i] * adjust]
+    ];
+
+    var pointMagAvg = [
+      [latitudes[i], longitudes[i] + horizontalError[i] * adjust],
+      [latitudes[i] + avgMag * 0.8, longitudes[i]],
+      [latitudes[i], longitudes[i] - horizontalError[i] * adjust]
+    ];
+
+    var pointDepthAvg = [
+      [latitudes[i], longitudes[i] + horizontalError[i] * adjust],
+      [latitudes[i] - avgDepth * 0.2, longitudes[i]],
+      [latitudes[i], longitudes[i] - horizontalError[i] * adjust]
+    ];
+
+    // var pointMag = [
+    //   [latitudes[i] + magnitudes[i] * 0.5, longitudes[i]],
+    //   [latitudes[i], longitudes[i]]
+    // ];
+    // var pointDepth = [
+    //   [latitudes[i], longitudes[i]],
+    //   [latitudes[i] - depths[i] * 0.2, longitudes[i]]
+    // ];
+    var colorRecent = palette.colorForValue(i);
+    var polygonMag = L.polyline(pointMag, {
+      color: "blue",
+      opacity: 0.8
+      // fill: colorRecent,
+      // fillOpacity: 0.5
+    }).addTo(mymap);
+    var polygonMagAvg = L.polyline(pointMagAvg, {
+      color: "blue",
+      opacity: 0.2,
+      className: "avg"
+      // fill: colorRecent,
+      // fillOpacity: 0.5
+    }).addTo(mymap);
+    var polygonDepth = L.polyline(pointDepth, {
+      color: "green",
+      opacity: 0.8
+      // fill: colorRecent,
+      // fillOpacity: 0.5
+    }).addTo(mymap);
+    var polygonDepthAvg = L.polyline(pointDepthAvg, {
+      color: "green",
+      opacity: 0.2,
+      className: "avg"
+      // fill: colorRecent,
+      // fillOpacity: 0.5
+    }).addTo(mymap);
+    var polygonHorL = L.polyline(horizontalLineLeft, { color: "black" }).addTo(
+      mymap
+    );
+    var polygonHorR = L.polyline(horizontalLineRight, { color: "black" }).addTo(
+      mymap
+    );
+
+    // // trigger click event
+    polygonDepth.on("click", onPolyClick);
+    polygonHorL.on("click", onPolyClick);
+    polygonHorR.on("click", onPolyClick);
+    polygonMag.on("click", onPolyClick);
+    polygonMagAvg.on("click", onPolyClick);
+    polygonDepthAvg.on("click", onPolyClick);
+  }
+
+  // click events
+  function onPolyClick() {
+    console.log("clicked!", this);
+    // console.log(this.options.opacity);
+
+    this.options.color = "red"; // updates value but doesnt change map
+    mymap.flyTo([this._latlngs[0].lat, this._latlngs[0].lng], 5);
+  }
+
+  function onCircleClick() {
+    // console.log("clicked!", this);
+
+    mymap.flyTo([this._latlng.lat, this._latlng.lng], 5);
+  }
+
+  // connect the events
   var polyline = L.polyline(latlngs, {
-    color: "lightblue",
-    opacity: 0.7,
+    color: "#3d3d3d",
+    opacity: 0.3,
     dashArray: "6",
     weight: 2
   }).addTo(mymap);
@@ -154,6 +314,7 @@ function drawDataPoints() {
   mymap.fitBounds(polyline.getBounds());
 }
 
+// not used...should call if making dynamic and updating on new data pull
 function removeAllCircles() {
   // remove each circle from the map and empty our array of references
   circles.forEach(function(circle, i) {
