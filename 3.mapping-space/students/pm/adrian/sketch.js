be41270df5ffaf1eac7b58abeb9e73f36ff39fc1
Adrian Crockett <adrian.s.crockett@gmail.com>
2018-11-14 15:39:24 -0500

    Assignment 3
    Push assignment 3

--- 3.mapping-space/students/pm/adrian/sketch.js
+++ 3.mapping-space/students/pm/adrian/sketch.js
@@ -4,26 +4,19 @@ var magnitudes;
 var depths;
 // an array for lat & long
 var latitudes, longitudes;
-
 // minimum and maximum values for magnitude and depth
 var magnitudeMin, magnitudeMax;
 var depthMin, depthMax;
-
 // the dots we'll be adding to the map
 var circles = [];
-
 // table as the data set
 var table;
-
 // my leaflet.js map
 var mymap;
 
 function preload() {
     // load the CSV data into our `table` variable and clip out the header row
-    table = loadTable("data/all_hour.csv", "csv", "header");
-    table = loadTable("data/all_day.csv", "csv", "header");
     table = loadTable("data/all_week.csv", "csv", "header");
-    table = loadTable("data/all_month.csv", "csv", "header");
 }
 
 function setup() {
@@ -31,15 +24,13 @@ function setup() {
     setupMap()
 
     // next, draw our p5 diagram that complements it
-    createCanvas(800, 600);
-    background(222);
+    createCanvas(1450, 100);
+    background(211);
 
     fill(0)
     noStroke()
-    textSize(16)
-    text(`Plotting ${table.getRowCount()} seismic events`, 20, 40)
-    text(`Largest Magnitude: ${getColumnMax("mag")}`, 20, 60)
-    text(`Greatest Depth: ${getColumnMax("depth")}`, 20, 80)
+    textSize(18)
+    text(`Over the course of the last week, there were ${table.getRowCount()} seismic events.  Of these events, the largest magnitude and depth were ${getColumnMax("mag")} and ${getColumnMax("depth")}, respectively`, 10, 36)
 }
 
 function setupMap(){
@@ -47,8 +38,7 @@ function setupMap(){
     LEAFLET CODE
 
     In this case "L" is leaflet. So whenever you want to interact with the leaflet library
-    you have to refer to L first.
-    so for example L.map('mapid') or L.circle([lat, long])
+    you have to refer to L first. so for example L.map('mapid') or L.circle([lat, long])
     */
 
     // create your own map
@@ -59,7 +49,8 @@ function setupMap(){
     L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
         attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
         maxZoom: 18,
-        id: 'mapbox.streets',
+        opacity: 0.9,
+        id: 'mapbox.light',
         accessToken: 'pk.eyJ1IjoiZHZpYTIwMTciLCJhIjoiY2o5NmsxNXIxMDU3eTMxbnN4bW03M3RsZyJ9.VN5cq0zpf-oep1n1OjRSEA'
     }).addTo(mymap);
 
@@ -68,7 +59,7 @@ function setupMap(){
 }
 
 function drawDataPoints(){
-    strokeWeight(5);
+    strokeWeight(1);
     stroke(255,0,0);
 
     // get the two arrays of interest: depth and magnitude
@@ -87,13 +78,13 @@ function drawDataPoints(){
     console.log('depth range:', [depthMin, depthMax])
 
     // cycle through the parallel arrays and add a dot for each event
-    for(var i=0; i<depths.length; i++){
+    for(var i=0; i<magnitudes.length; i++){
         // create a new dot
         var circle = L.circle([latitudes[i], longitudes[i]], {
-            color: 'red',      // the dot stroke color
-            fillColor: '#f03', // the dot fill color
-            fillOpacity: 0.25,  // use some transparency so we can see overlaps
-            radius: magnitudes[i] * 40000
+          color: 'black',      // the dot stroke color
+          fillColor: 'black', // the dot fill color
+          fillOpacity: 0.5,  // use some transparency so we can see overlaps
+          radius: magnitudes[i] * 10000
         });
 
         // place it on the map
@@ -112,7 +103,7 @@ function removeAllCircles(){
     circles = [];
 }
 
-// get the maximum value within a column
+// get the maximum value within a column in table
 function getColumnMax(columnName){
     // get the array of strings in the specified column
     var colStrings = table.getColumn(columnName);
@@ -132,4 +123,6 @@ function getColumnMax(columnName){
 
     // or do it the 'easy way' by using lodash:
     // return _.max(colValues);
+
+
 }
