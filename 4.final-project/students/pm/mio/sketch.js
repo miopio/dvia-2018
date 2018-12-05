3dd1e0bdc1204e5e52022e533f9ac226e6c22e7f
miopio <mioakasako@gmail.com>
2018-12-05 15:53:36 -0500

    final project progress

--- 4.final-project/students/pm/mio/sketch.js
+++ 4.final-project/students/pm/mio/sketch.js
@@ -3,7 +3,7 @@ var table;
 
 function preload() {
     // load the CSV data into our `table` variable and clip out the header row
-    table = loadTable("data/WHR2018Chapter2OnlineData", "csv", "header");
+    table = loadTable("data/happinessData2018.csv", "csv", "header");
 }
 
  function setup() {
@@ -18,14 +18,71 @@ function preload() {
     text(`Largest Magnitude: ${getColumnMax("mag")}`, 20, 60)
     text(`Greatest Depth: ${getColumnMax("depth")}`, 20, 80)*/
 
-    country = graphtable.getColumn('country');
-    year = graphtable.getColumn('year');
-    lifeladder = graphtable.getColumn('Life Ladder');
-    socialsupport = graphdepth.getColumn('Social support');
-    freedom = graphtable.getColumn('Freedom to make life choices');
-    generosity = graphtable.getColumn('Generosity');
-    corruption = graphtable.getColumn('Perceptions of corruption');
-    democracy = graphtable.getColumn('Democratic quality');
+    country = table.getColumn('country');
+    region = table.getColumn('Region indicator');
+    //year = table.getColumn('year');
+    lifeladder = table.getColumn('Life ladder, 2015-2017');
+    //lifeladder.sort(function(a, b){return b-a});
+    socialsupport = table.getColumn('Social support, 2015-2017');
+    freedom = table.getColumn('Freedom to make life choices, 2015-2017');
+    generosity = table.getColumn('Generosity, 2015-2017, without adjustment for GDP per person');
+    corruption = table.getColumn('Perceptions of corruption, 2015-2017');
+    //democracy = table.getColumn('Democratic quality');
+
+var data = [
+  {
+    x: country,
+    y: lifeladder,
+    type: 'bar'
+  }
+];
+
+Plotly.newPlot('myDiv', data);
+
+
+var data1 = [
+  {
+    x: country,
+    y: socialsupport,
+    type: 'bar'
+  }
+];
+
+Plotly.newPlot('myDiv1', data1);
+
+var data2 = [
+  {
+    x: country,
+    y: freedom,
+    type: 'bar'
+  }
+];
+
+Plotly.newPlot('myDiv2', data2);
+
+var data3 = [
+  {
+    x: country,
+    y: generosity,
+    type: 'bar'
+  }
+];
+
+Plotly.newPlot('myDiv3', data3);
+
+var data4 = [
+  {
+    x: country,
+    y: corruption,
+    type: 'bar'
+  }
+];
+
+Plotly.newPlot('myDiv4', data4);
 
 
 }
+
+
+
+
